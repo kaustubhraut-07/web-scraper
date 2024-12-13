a9dfc1883872d6911167141f7436e3c14c90ac81
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-12-13 11:44:17 +0530

    inserted data to elastic search

--- spider.py
+++ spider.py
@@ -1,9 +1,12 @@
 import scrapy
 import os
-
+from elasticsearch import Elasticsearch 
 
 # the structure and naming conventions are necessary as scrapy will automalty call the urls that we write in the start_urls
 # and after thet it will pass the flow to the parse function and alos it is necessry to write the function name as parse only
+
+
+# can get more details in this page  := https://docs.scrapy.org/en/latest/topics/spiders.html
 
 class FormSpider(scrapy.Spider):
     name = "form_spider"
@@ -16,9 +19,12 @@ class FormSpider(scrapy.Spider):
     def parse(self, response):
         if response.status == 200:
             full_html = response.text
-            self.logger.info(f"Fetched content with length: {len(full_html)}")
+            # self.logger.info(f"Fetched content with length: {len(full_html)}")
+            es = Elasticsearch(hosts=[{'host': '127.0.0.1', 'port': 9200 , "scheme": "http"}])
+            res = es.index(index='myindex', document={"content": full_html})
 
-            # Creating unique file naem 
+
+           
             url_parts = response.url.split('//')
             domain = url_parts[1].split('/')[0]
             filename = f"{domain.replace('.', '_')}.html"
@@ -26,10 +32,13 @@ class FormSpider(scrapy.Spider):
             try:
                 with open(filename, "w", encoding='utf-8') as f:
                     f.write(full_html)
-                self.logger.info(f"Content written to {filename}")
+                print("Content written to file successfully!")
+                # self.logger.info(f"Content written to {filename}")
             except Exception as e:
-                self.logger.error(f"Failed to write content to {filename}: {e}")
+                # self.logger.error(f"Failed to write content to {filename}: {e}")
+                print(f"Failed to write content to {filename}: {e}")
         else:
-            self.logger.error(f"Failed to fetch content. Status code: {response.status}")
+            # self.logger.error(f"Failed to fetch content. Status code: {response.status}")
+            print(f"Failed to fetch content. Status code: {response.status}")
 
         return None
